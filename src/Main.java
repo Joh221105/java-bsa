3a983d0e955c905b0c9177fc6c80909ec1ef76f1
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-09-15 21:04:11 -0400

    adds generateTarget method to generate a random intger between low and high user input for computer to search for

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
 package src;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int lowRange = getLowRange(scan);
+        int highRange = getHighRange(scan);
+        int result = binarySearch(lowRange, highRange);
+    }
+    private static int getLowRange(Scanner scan){
+        System.out.print("Please enter the low number: ");
+        return scan.nextInt();
+    }
 
+    private static int getHighRange(Scanner scan){
+        System.out.print("Please enter the high number: ");
+        return scan.nextInt();
+    }
+
+    private static int generateTarget(int low, int high) {
+        Random rand = new Random();
+        return rand.nextInt((high - low) + 1) + low;
+    }
+
+    private static int binarySearch(int low, int high){
+//        while(true){
+//
+//        }
+        return -1;
     }
 }
